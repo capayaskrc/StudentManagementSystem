3c411432bc0413be817a68d3627d99554daf5100
Kayl Rovic Capayas <kayl.capayas@valens-research.com>
2023-12-21 22:21:25 +0800

    change the fetch url endpoint, make the header and navbar reusable code. change the add course to course.php for compact for view course and add course

---
+++ layout/header.php
@@ -0,0 +1,9 @@
+<header class="header">
+
+    <a class="title" href="admin_dashboard.php">Dashboard</a>
+
+    <div class="logout">
+        <a href="../logout.php" class="btn btn-primary">Logout</a>
+    </div>
+
+</header>
---
+++ layout/navbar.php
@@ -0,0 +1,8 @@
+<aside>
+    <ul>
+        <li><a href="add_user.php">Add User</a></li>
+        <li><a href="">View Student</a></li>
+        <li><a href="add_course.php">Add Courses</a></li>
+        <li><a href="">View Courses</a></li>
+    </ul>
+</aside>
