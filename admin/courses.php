e5380591c84758b2f64478818831158f0d57f26c
Kayl Rovic Capayas <kayl.capayas@valens-research.com>
2023-12-30 08:41:54 +0800

    added ui on the courses.php, working add courses, view courses

--- admin/courses.php
+++ admin/courses.php
@@ -1,154 +1,169 @@
 <?php include '../layout/header_user.php'; ?>
 
-<section id="courses">
-    <h1>My Courses</h1>
+    <section id="courses" style="container-fluid justify-content-md-center col-md-8">
+        <h1>My Courses</h1>
+        <!-- Bootstrap button for adding courses -->
+        <button class="btn btn-primary" data-toggle="modal" data-target="#addCourseModal" onclick="openAddCourseModal()">Add Course</button>
 
-    <!-- Table to display courses -->
+        <!-- Bootstrap Modal -->
+        <div class="modal fade" id="addCourseModal" tabindex="-1" role="dialog" aria-labelledby="addCourseModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addCourseModalLabel">Add Course</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Add your form fields for adding a course -->
+                        <div class="form-group">
+                            <label for="courseName">Course Name</label>
+                            <input type="text" class="form-control" id="courseName" placeholder="Enter Course Name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" onclick="addCourse()">Add Course</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="closeAddCourseModal()">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <!-- Bootstrap Table -->
+        <div class="table-responsive">
+            <table class="table table-bordered" id="courseTable">
+                <thead>
+                <tr>
+                    <th>Course ID</th>
+                    <th>Course Name</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <!-- Course data will be displayed here dynamically -->
+                </tbody>
+            </table>
+        </div>
+    </section>
+    <script>
 
-    <!-- Buttons for adding and deleting courses -->
-    <button onclick="openAddCourseModal()">Add Course</button>
-    <!-- Add Course Modal -->
-    <div id="addCourseModal" style="display: none;">
-        <!-- Add your form fields for adding a course -->
-        <input type="text" id="courseName" placeholder="Enter Course Name">
-        <button onclick="addCourses()">Add Course</button>
-        <button onclick="closeAddCourseModal()">Close</button>
-    </div>
+        document.addEventListener("DOMContentLoaded", function () {
+            fetchAllCourses();
+        });
 
-    <table border="1" id="courseTable">
-        <thead>
-        <tr>
-            <th>Course ID</th>
-            <th>Course Name</th>
-            <th>Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        <!-- Course data will be displayed here dynamically -->
-        </tbody>
-    </table>
-
-</section>
-<script>
-
-    // TODO: add search bar to find the courses
-    //       make the add courses will be in different frame so that it will be modal truelly.
-    document.addEventListener("DOMContentLoaded", function () {
-        fetchAllCourses();
-    });
-
-
-    // Function to open the "Add Course" modal
-    function openAddCourseModal() {
-        document.getElementById('addCourseModal').style.display = 'block';
-    }
-
-    // Function to close the "Add Course" modal
-    function closeAddCourseModal() {
-        document.getElementById('addCourseModal').style.display = 'none';
-    }
-
-    function addCourses() {
-        // Get the course name from the input field
-        const courseName = document.getElementById("courseName").value;
-        if (courseName.trim() === "") {
-            alert("Please enter a course name");
-            return;
+        // Function to open the "Add Course" modal
+        function openAddCourseModal() {
+            document.getElementById('addCourseModal').style.display = 'block';
         }
 
-        const courseData = {
-            courseName: courseName,
-        };
-
-        // Call the addCourse function with courseData
-        addCourse(courseData);
-    }
-// Function to add a new course
-function addCourse(courseData) {
-    fetch('../auth/api.php?course', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // Add any other headers as needed (e.g., authentication headers)
-        },
-        body: JSON.stringify(courseData),
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to add course');
+        // Function to close the "Add Course" modal
+        function closeAddCourseModal() {
+            document.getElementById('addCourseModal').style.display = 'none';
+            // Manually remove the modal backdrop
+            document.body.classList.remove('modal-open');
+            const modalBackdrops = document.getElementsByClassName('modal-backdrop');
+            for (let backdrop of modalBackdrops) {
+                backdrop.parentNode.removeChild(backdrop);
             }
-            return response.json();
-        })
-        .then(data => {
-            closeAddCourseModal();
-        })
-        .catch(error => {
-            console.error('Error adding course:', error);
-            // Handle error as needed (e.g., display an error message to the user)
-        });
-}
+        }
 
-    function fetchAllCourses() {
-        // Make an API call to get all courses
-        fetch('../auth/api.php?courses', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch courses');
-                }
-                return response.json();
+        function addCourse() {
+            const courseName = document.getElementById("courseName").value;
+            if (courseName.trim() === "") {
+                alert("Please enter a course name");
+                return;
+            }
+            const courseData = {
+                courseName: courseName,
+            };
+            fetch('../auth/api.php?course', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(courseData),
             })
-            .then(data => {
-                allCourses = data;
-                populateCourseTable(allCourses);
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to add course');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    fetchAllCourses(); // Refresh the course list after adding a new course
+                    closeAddCourseModal();
+                })
+                .catch(error => {
+                    console.error('Error adding course:', error);
+                    // Handle error as needed (e.g., display an error message to the user)
+                });
+        }
+
+        function fetchAllCourses() {
+            fetch('../auth/api.php?courses', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             })
-            .catch(error => {
-                console.error('Error fetching courses:', error);
-                // Handle error as needed (e.g., display an error message to the user)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch courses');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    populateCourseTable(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching courses:', error);
+                    // Handle error as needed (e.g., display an error message to the user)
+                });
+        }
+
+        function populateCourseTable(courses) {
+            const courseTable = document.getElementById('courseTable');
+            const tbody = courseTable.getElementsByTagName('tbody')[0];
+            tbody.innerHTML = ''; // Clear existing rows
+
+            courses.forEach(course => {
+                const row = tbody.insertRow();
+                row.insertCell(0).textContent = course.course_id;
+                row.insertCell(1).textContent = course.course_name;
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Delete';
+                deleteButton.className = 'btn btn-danger btn-sm';
+                deleteButton.onclick = function () {
+                    deleteCourse(course.course_id);
+                };
+                const cell = row.insertCell(2);
+                cell.appendChild(deleteButton);
             });
-    }
+        }
 
-    function populateCourseTable(courses) {
-        const courseTable = document.getElementById('courseTable');
-        const tbody = courseTable.getElementsByTagName('tbody')[0];
-        tbody.innerHTML = ''; // Clear existing rows
-
-        courses.forEach(course => {
-            const row = tbody.insertRow();
-            row.insertCell(0).textContent = course.course_id;
-            row.insertCell(1).textContent = course.course_name;
-
-            const updateButton = document.createElement('button');
-            updateButton.textContent = 'Update';
-            updateButton.onclick = function () {
-                updateCourse(course);
-            };
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.onclick = function () {
-                deleteCourse(course.course_id);
-            };
-
-            row.insertCell(2).appendChild(updateButton);
-            row.insertCell(3).appendChild(deleteButton);
-        });
-    }
-
-    function updateCourse(course) {
-        // Implement logic for updating a course
-        console.log('Update course:', course);
-    }
-
-    function deleteCourse(courseId) {
-        // Implement logic for deleting a course
-        console.log('Delete course with ID:', courseId);
-    }
-
-</script>
+        function deleteCourse(courseId) {
+            if (confirm("Are you sure you want to delete this course?")) {
+                fetch(`../auth/api.php?course=${courseId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Failed to delete course');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        fetchAllCourses(); // Refresh the course list after deleting a course
+                    })
+                    .catch(error => {
+                        console.error('Error deleting course:', error);
+                        // Handle error as needed (e.g., display an error message to the user)
+                    });
+            }
+        }
+    </script>
 <?php include '../layout/footer.php'; ?>
--- auth/db_connection.php
+++ auth/db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "sms_db";
+$dbname = "sms_db_1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
