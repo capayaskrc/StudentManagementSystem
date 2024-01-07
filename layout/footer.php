76bb9f961436a793efe30bdac7cb2f6ab616cb30
Kayl Rovic Capayas <kayl.capayas@valens-research.com>
2024-01-07 20:59:36 +0800

    update the footer.php

--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,5 @@
 </div>
+</body>
 <!--<script src="js/script.js"></script>-->
 <script src="../js/auto_timeout.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
